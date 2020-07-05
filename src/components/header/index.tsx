5bb0c02f48564c9b8fe3f2bb6d8de5196c960e04
huangyuan3h <huangyuan3h@gmail.com>
2020-07-05 23:36:51 +0800

    add structure of header

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,7 +1,15 @@
 interface HeaderType {}
 
 const Header: React.FC<HeaderType> = ({}: HeaderType) => {
-  return <div className="header"></div>;
+  return (
+    <div className="header">
+      <div className="content">
+        <div className="logo-area">Logo</div>
+        <div className="search-area">search</div>
+        <div className="right-area">right</div>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,9 +1,13 @@
 import React from "react";
-
+import Header from "../../components/header";
 import "./styles/index.scss";
 
 const index: React.FC<{}> = () => {
-  return <div className="index">index pages</div>;
+  return (
+    <div className="index">
+      <Header />
+    </div>
+  );
 };
 
 export default index;
